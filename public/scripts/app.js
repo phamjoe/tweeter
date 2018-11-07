1ba6b0ddc628d147c903ef7eeb828a7162766ea6
Joseph Pham <j.pham017@gmail.com>
2018-11-07 09:22:29 -0500

    added error animations

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,81 +6,98 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 
-$(document).ready(function() {
-//var $tweet = createTweetElement(data);
 
-// Test / driver code (temporary)
-//console.log($tweet); // to see what it looks like
-//$('.container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+$(document).ready(function() {
+  const $compose = $('.compose');
+  const $error = $('.error-msg');
+  $error.hide();
+  $compose.hide();
+
+  $('.compose-btn').click(function(){
+    $compose.slideToggle('fast', function(){});
+    $('.tweet-text').focus();
+  });
 
   function createTweetElement(tweet) {
-    let $tweet = $('.tweet-list ').append($('<article>')
-                      .append($('<header>')
-                      .append($('<h2>')
-                        .append($('<img src=\"'+ tweet.user.avatars.small + '\"/>'))
-                        .append($('<span>' + tweet.user.name + '</span>').addClass('fullname'))
-                        .append($('<span>' + tweet.user.handle + '</span>').addClass('handle'))))
-                      .append($('<div>' + tweet.content.text + '</div>').addClass('tweet'))
-                      .append($('<footer>' + tweet.created_at + '</footer>')
-                        .append($('<a href=\"'+ 'http://www.youtube.ca' + '\">').addClass('foot-action')
-                          .append('<i class=\"fas fa-flag\"></i></a>'))
-                        .append($('<a href=\"'+ 'http://www.google.ca' + '\">').addClass('foot-action')
-                          .append('<i class=\"fas fa-retweet\"></i></a>'))
-                        .append($('<a href=\"'+ 'http://www.facebook.com' + '\">').addClass('foot-action')
-                          .append('<i class=\"fas fa-heart\"></i></a>'))));
-
-    // $tweet.find($(".profile-pic").attr(tweet.user.avatars.small));
+    let escapeText = $('<div>').text(tweet.content.text);
+    let $tweet = $('.tweet-list ').append(
+      $('<article>')
+        .append(
+          $('<header>').append(
+            $('<h2>')
+              .append($('<img src="' + tweet.user.avatars.small + '"/>'))
+              .append($('<span>' + tweet.user.name + '</span>').addClass('fullname'))
+              .append($('<span>' + tweet.user.handle + '</span>').addClass('handle'))
+          )
+        )
+        .append($(escapeText).addClass('tweet'))
+        .append(
+          $('<footer>' + tweet.created_at + '</footer>')
+            .append(
+              $('<a href="' + 'http://www.youtube.ca' + '">')
+                .addClass('foot-action')
+                .append('<i class="fas fa-flag"></i></a>')
+            )
+            .append(
+              $('<a href="' + 'http://www.google.ca' + '">')
+                .addClass('foot-action')
+                .append('<i class="fas fa-retweet"></i></a>')
+            )
+            .append(
+              $('<a href="' + 'http://www.facebook.com' + '">')
+                .addClass('foot-action')
+                .append('<i class="fas fa-heart"></i></a>')
+            )
+        )
+    );
     return $tweet;
   }
 
-function renderTweets(tweets) {
-  // loops through tweets
+  function renderTweets(tweets) {
+    // loops through tweets
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
-  let $value = tweets.forEach(function(key){
+    let $value = tweets.forEach(function(key) {
       return $('.container').append(createTweetElement(key));
-  })
+    });
+  }
 
-}
+  function loadTweets() {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+      success: function(getTweets) {
+        renderTweets(getTweets);
+      }
+    });
+  }
 
-function loadTweets(){
-  $.ajax({
-    method:"GET",
-    url: "/tweets",
-    success: function(getTweets){
-      renderTweets(getTweets);
+  loadTweets();
+
+  $('form').on('submit', function(event) {
+
+    event.preventDefault();
+
+    //TODO: check string length not serialized length
+    let stringData = $('form').children('textarea');
+    let data = $(this).serialize();
+    console.log(stringData);
+    if ($('textarea', this).val().trim() === '') {
+      $error.slideDown();
+    } else if (stringData.length > 140) {
+        alert('Character length exceeded');
+    } else {
+      $error.hide();
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: data
+      }).done(function() {
+        $('form')[0].reset();
+        $('.counter').html(140);
+        loadTweets();
+      });
     }
   });
-}
-
-loadTweets();
-  $('form').on('submit', function(event) {
-    event.preventDefault();
-    let data = $(this).serialize();
-    if($('textarea', this).val().trim() === ''){
-      alert("Empty values not allowed");
-    }
-    else if(data.length > 140){
-      alert("Character length exceeded");
-    }
-    else{
-      $.ajax({
-        method: "POST",
-        url: "/tweets",
-        data: data
-      })
-      .done(function(){
-        $('form')[0].reset();
-        loadTweets();
-      })
-    }
-  })
-})
-
-$(function(){
-
-})
-
-
-
+});
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  $( 'textarea' ).keyup(count);
-  $( 'textarea' ).keydown(count);
-  //$( 'textarea' ).on('input', count());
-  function count(){
-    var value = $( this ).val().length;
-    var counter = $(this).siblings('.counter');
-
-
-    counter.text(140 - value);
-    if(counter.text() < 0){
+  //$( 'textarea' ).keyup(count);
+  //$( 'textarea' ).keydown(count);
+  $( 'textarea' ).on('input', function(){
+  //function count(){
+    let $value = $( this ).val().length;
+    let $counter = $(this).siblings('.counter');
+    let $button = $(this).parent().find('input');
+    $counter.text(140 - $value);
+    if($counter.text() < 0){
       //counter.css('color', '#ff0000');
-      counter.addClass('counter-invalid-msg');
+      $counter.addClass('counter-invalid-msg');
+      $button.prop('disabled', true);
     }
     else{
       //counter.css('color', '#000000');
-      counter.removeClass('counter-invalid-msg');
+      $counter.removeClass('counter-invalid-msg');
+      $button.prop('disabled', false);
     }
-  }
-
-})
+  })
+});
